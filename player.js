dd5a561b5b09b87842ee6675f919ccff99fd24e7
Sergey Orlov <orlovMail@gmail.com>
2015-09-27 12:29:57 +0300

    remove old code

--- player.js
+++ player.js
@@ -15,23 +15,23 @@ function rank2Num(rank) {
  *      0 - если нужно сбросить карты
  */
 function preflop_stage(game_state) {
-  
+
     var player = game_state.players[game_state.in_action];
 
     if ( player.hole_cards.length != 2 ) {
       return false;
     }
-    
+
     var card1 = player.hole_cards[0], card2 = player.hole_cards[1];
-    
+
     if (
       card1.rank == card2.rank
     ) {
       return 1000;
     }
-    
+
     var bet = 0;
-    
+
     if ( rank2Num(card1.rank) >= 10 ) {
       bet += 500;
     }
@@ -61,8 +61,8 @@ function river_request(game_state) {
 module.exports = {
 
   exception: false,
-  VERSION: "Stage preflop, 2 strategy",  
-  
+  VERSION: "Stage preflop, 2 strategy",
+
   bet_request: function(game_state) {
     try {
       console.log('game_state_json', JSON.stringify(game_state));
@@ -93,34 +93,9 @@ module.exports = {
       return 1000;
     }
   },
-  
+
   showdown: function(game_state) {
 
   },
-
-  should_bet_after_flop: function(cards) {
-
-    return true;
-  },
-
-  any_cards_are_equal: function(cards) {
-    for (var i = 0; i < cards.length-1; i++) {
-      for (var j = i+1; j < cards.length; j++) {
-        if (cards[i].rank == cards[j].rank)
-          return true;
-      }
-    }
-    return false;
-  },
-
-  get_player_cards: function(game_state) {
-    var index = game_state.in_action;
-
-    if (game_state.players[index]) {
-      return game_state.players[index].hole_cards;
-    }
-
-    console.log("player not found", game_state);
-  }
 
 };

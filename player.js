ed2b2236e05a47c76f739b82221f77684ba60c18
Sergey Orlov <orlovMail@gmail.com>
2015-09-27 10:43:44 +0300

    game state to console.log

--- player.js
+++ player.js
@@ -4,7 +4,7 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state) {
-    console.log("game_state")
+    console.log("game_state !!!", game_state)
     return 1000;
   },
 

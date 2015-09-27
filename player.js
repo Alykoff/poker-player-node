b3ca51aecfb339df3656d3e0cdd6074495e14419
Michael Akimov <michael@island-future.ru>
2015-09-27 11:03:47 +0300

    preflop

--- player.js
+++ player.js
@@ -2,12 +2,57 @@
 module.exports = {
 
   VERSION: "Default JavaScript folding player",
-
+  
   bet_request: function(game_state) {
-    console.log("game_state !!!", game_state)
+    console.log("game_state !!!", game_state);
     return 1000;
   },
 
+  /**
+   * @returns false - если не префлоп
+   *      1000 - если пара или больше или равно 10
+   *      0 - если нужно сбросить карты
+   */
+  preflop_stage: function(game_state) {
+    var player = game_state.players[game_state.in_action];
+    
+    if ( game_state.community_cards && game_state.community_cards.length == 0 ) {
+      return false;
+    }
+    
+    if ( player.hole_cards.length != 2 ) {
+      return false;
+    }
+    
+    var card1 = player.hole_cards[0], card2 = player.hole_cards[1];
+    
+    if (
+      card1.rank == card2.rank
+    ) {
+      return 1000;
+    }
+    
+    if (
+      (
+        card1.rank == '10'
+        || card1.rank == 'J'
+        || card1.rank == 'Q'
+        || card1.rank == 'K'
+        || card1.rank == 'A'
+      ) && (
+        card2.rank == '10'
+        || card2.rank == 'J'
+        || card2.rank == 'Q'
+        || card2.rank == 'K'
+        || card2.rank == 'A'
+      )
+    ) {
+      return 1000;
+    }
+    
+    return 10;
+  },
+  
   showdown: function(game_state) {
 
   }

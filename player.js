ed6289107c7d70e79d971547deeaab6415c84377
kesot <kesot@mail.ru>
2015-09-27 11:06:30 +0300

    11

--- player.js
+++ player.js
@@ -57,6 +57,10 @@ module.exports = {
 
   },
 
+  should_bet_after_flop: function(cards) {
+    return true;
+  }
+
   get_player_cards: function(game_state) {
     var index = game_state.in_action;
 
